61845224cda90c30f82333ab57738ecf035fd747
b1thunt3r <ishan@jain.se>
2019-02-06 10:15:09 +0000

    Added common.props

--- nuget.cmd
+++ nuget.cmd
@@ -1,16 +1,16 @@
 echo off
 
-SET nugetDir=%3
+SET nugetDir="P:\Nuget\"
 echo ## Create Nuget package for %1 (%2) in %nugetDir%
+
+::echo ## Check if Nuget Symbols directory exists, otherwise create one
+set nugetSymbolsDir=%nugetDir%\symbols
+IF NOT EXIST %nugetSymbolsDir% mkdir %nugetSymbolsDir%
 
 ::echo ## Create packages
 dotnet pack %1 --no-build --configuration %2 --output %nugetDir% --include-symbols --verbosity minimal
 
-::echo ## Check if Nuget Symbols directory exists, otherwise create one
-set nugetSymbolsDir=%nugetDir%\symbols
-
 ::echo ## Move Nuget Symbols, to Symbols directory
-IF NOT EXIST %nugetSymbolsDir% mkdir %nugetSymbolsDir%
 move %nugetDir%\*.symbols.nupkg %nugetSymbolsDir%
 
 ::echo ## Complete
