ee9abafde8162b514113eb535d686589f7da8005
b1thunt3r <ishan@jain.se>
2019-02-12 15:22:27 +0000

    Fix PluginsFolderNotFound for linux.

--- tests/PluginTests/PluginLoaderTests.cs
+++ tests/PluginTests/PluginLoaderTests.cs
@@ -32,13 +32,13 @@ namespace PluginTests
         [Fact]
         public void PluginsFolderNotFound()
         {
-            var path = new DirectoryInfo($"X:\\{DateTime.Now.ToBinary().ToString()}");
+            var path = new DirectoryInfo(DateTime.Now.ToBinary().ToString());
             Action action = () => new PluginLoader(path, _logger);
 
             action.Should().Throw<DirectoryNotFoundException>();
 
             _logger.Last.EventId.Id.Should().Be(4004);
-            _logger.Last.Message.Should().StartWith($"Could not find {path}");
+            _logger.Last.Message.Should().StartWith($"Could not find {path.FullName}");
         }
 
         [Fact]
